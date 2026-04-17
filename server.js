2ec29fdaf387c5da71fd7a152dee5635d0af6825
infantakash210705 <infantakash260@gmail.com>
2026-04-17 12:19:44 +0530

    connected to railway db

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 let db;
 
 async function initDB() {
